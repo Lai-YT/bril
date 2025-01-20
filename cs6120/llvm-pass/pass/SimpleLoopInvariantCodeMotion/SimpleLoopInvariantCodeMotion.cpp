31a5bba69db51179e1eea967c03c52d892b878a7
Lai-YT <381xvmvbib@gmail.com>
2025-01-20 14:20:25 +0800

    Check if the loop is in candidate form

--- cs6120/llvm-pass/pass/SimpleLoopInvariantCodeMotion/SimpleLoopInvariantCodeMotion.cpp
+++ cs6120/llvm-pass/pass/SimpleLoopInvariantCodeMotion/SimpleLoopInvariantCodeMotion.cpp
@@ -1,5 +1,6 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -13,14 +14,52 @@ namespace {
 constexpr char PassName[] = "slicm";
 constexpr char PluginName[] = "Simple Loop Invariant Code Motion";
 
+class SimpleLoopInvariantCodeMotionImpl {
+public:
+  SimpleLoopInvariantCodeMotionImpl(LoopInfo &LI) : LI(LI) {}
+
+  bool run(Loop &L) const;
+
+  /// Check if the loop \p L is in the candidate form.
+  bool isCandidate(Loop &L) const;
+
+private:
+  LoopInfo &LI;
+};
+
 class SimpleLoopInvariantCodeMotionPass
     : public PassInfoMixin<SimpleLoopInvariantCodeMotionPass> {
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    errs() << "Simple Loop Invariant Code Motion @ " << F.getName() << "\n";
-    return PreservedAnalyses::all();
+    errs() << "SLICM @ " << F.getName() << "\n";
+    auto &LI = FAM.getResult<LoopAnalysis>(F);
+    SimpleLoopInvariantCodeMotionImpl Impl(LI);
+    bool Changed = false;
+    for (Loop *L : LI) {
+      Changed |= Impl.run(*L);
+    }
+    return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 };
+
+bool SimpleLoopInvariantCodeMotionImpl::run(Loop &L) const {
+  if (!isCandidate(L))
+    return false;
+
+  errs() << "SLICM @ " << L.getHeader()->getName() << "\n";
+  // TODO: Implement the actual transformation.
+  return true;
+}
+
+bool SimpleLoopInvariantCodeMotionImpl::isCandidate(Loop &L) const {
+  // SLICM moves code to the preheader, so the loop must have a preheader.
+  if (!L.isLoopSimplifyForm()) {
+    errs() << "Loop is not in loop simplify form\n";
+    return false;
+  }
+
+  return true;
+}
 
 } // namespace
 
